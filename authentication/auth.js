260019ed66d6418cfc9d81e0b72bd9ee4e33bdd2
sreekanthkumarraju <rvsreekanthkumar90@gmail.com>
2022-07-02 09:11:09 +0530

    changes

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,5 +1,5 @@
-const bcrypt=require('bcrypt')
 
+const bcrypt = require('../index').bcrypt
 let rounds=10
 const hashPassword=async (password)=>{
     let salt=await bcrypt.genSalt(rounds)
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const bcrypt=require('bcrypt')
 const express=require('express')
 const app=express()
 const bodyParser=require('body-parser')
